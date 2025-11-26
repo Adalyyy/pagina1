25f9ba6f814895817c82743928329d8c4223519f
Fatima Sanchez <fatimaadalys@gmail.com>
2025-11-26 08:35:54 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include 'header.php';?>
+<?php include 'menu.php';?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <p>Este es el index</p>
+            <!-- parametros -->
+            <!-- para agregar más parametros &
+                info.php?materia=pw&semestre=5  -->
+            <a href="info.php?materia=pw&semestre=5">PW</a>
+            <a href="info.php?materia=back&semestre=7">Backend</a>
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php';?>
